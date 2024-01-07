9b6e5938509a2fbde7e61e9e6c69d022a633210f
Rayanne <mouraribeiro11@gmail.com>
2024-01-07 17:11:52 -0300

    configurações iniciais

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from core.config import settings
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+
+@app.on_event("startup")
+async def app_init():
+    cliente_db= AsyncIOMotorClient(
+        settings.MONGO_CONNECTION_STRING
+    ).storeapp
+
+    await init_beanie(
+        database=cliente_db,
+        document_models=[
+
+        ]
+    )
